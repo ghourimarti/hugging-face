d099f17a53d0a206a0cd50bb3b41d1156b502bc3
ghourimarti <ghourimarti@gmail.com>
2025-03-18 10:37:06 +0000

    new demo hugging face

---
+++ s.sh
@@ -0,0 +1,19 @@
+clear
+virtualenv ~/.venv
+vim ~/.bashrc
+source ~/.venv/bin/activate
+
+
+
+
+# make install
+# make test
+# make format
+# make lint
+# make all
+
+# #
+# git status
+# git add *
+# git commit -m "adding initial layout of python project"
+# git push
